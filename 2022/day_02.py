e3ee383f9fe85f236993da671b8167cbae17d907
Adam Matan <adam@matan.name>
2022-12-26 18:57:36 +0200

    Day 02 part 1 - WIP

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -1,51 +1,68 @@
-def get_normalized_input(filename='day_02_input.txt'):
+from enum import Enum
+
+class PaperRockScissors():
+
+    class Gesture(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
+
+    class MatchScores(Enum):
+        LOSE = 0
+        DRAW = 3
+        WIN = 6
+
+    _codes_to_gestures = {
+        'A': Gesture.ROCK,
+        'X': Gesture.ROCK,
+        'B': Gesture.PAPER,
+        'Y': Gesture.PAPER,
+        'C': Gesture.SCISSORS,
+        'Z': Gesture.SCISSORS,
+    }
+
+    def __init__(self, gesture_code: str) -> None:
+        if gesture := PaperRockScissors._codes_to_gestures:
+            self.gesture = PaperRockScissors._codes_to_gestures[gesture_code]
+        else:
+            raise ValueError(f'Gesture code {gesture_code} is not a valid paper, rock, scissors code')
+
+    def get_score(self, other):
+        if self.gesture == other.gesture:
+            return self.gesture.value + PaperRockScissors.MatchScores.DRAW.value
+        if (self.gesture == PaperRockScissors.Gesture.ROCK and other.gesture == PaperRockScissors.Gesture.SCISSORS) \
+            or (self.gesture == PaperRockScissors.Gesture.PAPER and other.gesture == PaperRockScissors.Gesture.ROCK) \
+            or (self.gesture == PaperRockScissors.Gesture.SCISSORS and other.gesture == PaperRockScissors.Gesture.PAPER):
+            return self.gesture.value + PaperRockScissors.MatchScores.WIN.value
+        else:
+            return self.gesture.value + PaperRockScissors.MatchScores.LOSE.value
+
+    def __str__(self) -> str:
+        return f'{self.gesture.name}'
+
+
+def parse_input(filename='day_02_input.txt'):
     # Parses the input file into a list of two-letters matches, e.g. ['AY', 'CZ', ...]
     with open(filename) as f:
         lines = [l for l in f.readlines() if l.strip()]
-        lines = [l.strip().split() for l in lines]
-        lines = [''.join(l) for l in lines]
+    lines = [l.strip().split() for l in lines]
+    lines = [''.join(l) for l in lines]
     return lines
 
-def calculate_game_score(two_letters_game):
-    shape_values = {
-        'A': 1,
-        'B': 2,
-        'C': 3
-    }
-    shape_normalization = {
-        'X': 'A',
-        'Y': 'B',
-        'Z': 'C'
-    }
-    scores = {
-        'lose': 0,
-        'draw': 3,
-        'win': 6
-    }
-    oponent_move, my_move = two_letters_game
-    my_move = shape_normalization[my_move]
-
-    if oponent_move == my_move:
-        score = shape_values[my_move] + scores['draw']
-        print(f'Draw {two_letters_game} {oponent_move} {my_move} {score}')
-        return score
-    if oponent_move+my_move in ('AB', 'BC', 'CA'):
-        score = shape_values[my_move] + scores['win']
-        print(f'Win  {two_letters_game} {oponent_move} {my_move} {score}')
-        return score
-    else:
-        score = shape_values[my_move] + scores['lose']
-        print(f'Lose {two_letters_game} {oponent_move} {my_move} {score}')
-        return score
-
-
 def part_1():
-    games = get_normalized_input()
-    scores = [calculate_game_score(game) for game in games]
-    return sum(scores)
-
+    games = parse_input()
+    scores = 0
+    for game in games:
+        oponent_move = PaperRockScissors(game[0])
+        my_move = PaperRockScissors(game[1])
+        scores += my_move.get_score(oponent_move)
+    return scores
 
 def main():
+    """
+    >>> part_1()
+    14375
+    """
     print(part_1())
 #    print(part_2())
 
